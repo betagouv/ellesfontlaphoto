62be18d8e4d1279510e94a929c7fc6701b15cbf0
Nadia Auger <nadia.auger0@gmail.com>
2021-09-09 17:00:36 +0200

    sendinblue

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,6 +3,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contacts_params)
     if @contact.save
       if @contact.contact_type = "newsletter"
+        add_to_sendinblue_list(@contact.email)
         redirect_to root_path(anchor: 'contact'), alert: 'Votre demande a bien été prise en compte.'
       else
         redirect_to root_path(anchor: 'contact'), notice: 'Votre demande a bien été prise en compte.'
@@ -17,11 +18,29 @@ class ContactsController < ApplicationController
   end
 
   private
+
   def contacts_params
     params.require(:contact).permit(
       :email,
       :contact_type,
       :message
       )
+  end
+
+  def add_to_sendinblue_list(email)
+    # SendInBlue : put email address in List #57[Newsletter]
+    url = URI('https://api.sendinblue.com/v3/contacts')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    email = email
+    list_id = 57
+    request = Net::HTTP::Post.new(url)
+    request['accept'] = 'application/json'
+    request['content-type'] = 'application/json'
+    request['api-key'] = Figaro.env.sendinblue_api_key
+    request.body = "{\"email\":\"#{email}\",\"listIds\":[#{list_id}],\"updateEnabled\":true}"
+    response = http.request(request)
+    puts response.read_body
   end
 end
