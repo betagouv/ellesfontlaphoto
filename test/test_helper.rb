d2f695ac3382c94e5984cd727155feadbef37ebf
Nadia Auger <nadia.auger0@gmail.com>
2021-09-13 13:58:42 +0200

    test github actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,23 +8,16 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  p "ici 3"
-
   # Add more helper methods to be used by all tests here...
   # Devise test helpers
   include Warden::Test::Helpers
   Warden.test_mode!
-  p "ici 2"
 end
 
-p "ici"
 # Register the new driver for Capybara
 Capybara.register_driver :headless_chrome do |app|
-  p "ici 1"
   options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu window-size=1400,900])
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-  p "ici 4"
 end
 Capybara.save_path = Rails.root.join('tmp/capybara')
 Capybara.javascript_driver = :headless_chrome
