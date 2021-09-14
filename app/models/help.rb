e9e2a263d8f5876265757e66bacdd740bbb836cd
Nadia Auger <nadia.auger0@gmail.com>
2021-09-14 12:40:22 +0200

    help show

---
+++ app/models/help.rb
@@ -0,0 +1,69 @@
+class Help < ApplicationRecord
+  HELP_TITLE = {
+    "AIC": "Aide individuelle à la création | AIC",
+    "AIA": "Aide à l'installation d'atelier et à l'achat de matériel | AIC",
+  }
+
+  HELP_TYPE = {
+    "Matériel": "Achat de matériel",
+    "Production": "Aide à la production",
+    "Autre": "Aide diverses"
+  }
+
+  HELP_RESIDENCE = [
+    "Provence-Alpes-Côte-d'Azur",
+    "Pays de la Loire",
+    "Occitanie",
+    "Nouvelle Aquitaine",
+    "Normandie",
+    "Ile de France",
+    "Hauts de France",
+    "Grand-Est",
+    "Corse",
+    "Centre Val de Loire",
+    "Bretagne",
+    "Bourgogne -Franche Comté",
+    "Auvergne-Rhône Alpes",
+    "Réunion",
+    "Mayotte",
+    "Martinique",
+    "Guyane",
+    "Guadeloupe",
+  ]
+
+  HELP_MONTH = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "août",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre",
+  ]
+
+  HELP_MONTH_TRANSLATE = {
+    "janvier": "jan",
+    "février": "feb",
+    "mars": "mar",
+    "avril": "apr",
+    "mai": "may",
+    "juin": "june",
+    "juillet": "jul",
+    "août": "aug",
+    "septembre": "sept",
+    "octobre": "oct",
+    "novembre": "nov",
+    "décembre": "dec",
+  }
+
+  validates :residence_condition, inclusion: { in: Help::HELP_RESIDENCE }
+  validates :start_month, inclusion: { in: Help::HELP_MONTH }
+  validates :end_month, inclusion: { in: Help::HELP_MONTH }
+  validates :title, inclusion: { in: ["AIC", "AIA"] }
+  validates :help_type, inclusion: { in: ["Matériel", "Production"] }
+end
---
+++ db/migrate/20210913150842_create_helps.rb
@@ -0,0 +1,30 @@
+class CreateHelps < ActiveRecord::Migration[6.1]
+  def change
+    create_table :helps do |t|
+      t.string :title
+      t.string :description
+      t.string :sector
+      t.string :structure_name
+      t.string :start_month
+      t.string :end_month
+      t.string :help_amount
+      t.string :residence_condition
+      t.string :general_condition
+      t.text :specific_condition
+      t.string :candidate_url
+      t.string :institution_url
+      t.text :selection
+      t.string :compo_commission
+      t.string :url_commission
+      t.string :old_laureat
+      t.string :old_laureat_url
+      t.string :help_type
+      t.text :admin_attachment
+      t.text :artistic_attachment
+      t.text :other_attachment
+      t.text :contact_institution
+      t.string :contact_institution_url
+      t.timestamps
+    end
+  end
+end
