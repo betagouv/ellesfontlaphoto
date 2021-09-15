c33f39e3b2a960a0930c91a1cb7bbfece207dad0
Nadia Auger <nadia.auger0@gmail.com>
2021-09-15 10:27:21 +0200

    add index

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,5 +1,9 @@
 class HelpsController < ApplicationController
 
+  def index
+    @helps = Help.all
+  end
+
   def show
     @help = Help.find(params[:id])
     if Help::HELP_MONTH_TRANSLATE[@help.start_month.to_sym].to_date < Date.today && Date.today < Help::HELP_MONTH_TRANSLATE[@help.end_month.to_sym].to_date.end_of_month
