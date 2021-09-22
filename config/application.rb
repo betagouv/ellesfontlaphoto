6cf70c2bd401cbe60f9f1f4e75ef3e7dedfd0f7a
Nadia Auger <nadia.auger0@gmail.com>
2021-09-22 21:35:36 +0200

    seed done

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Ellesfontlaphoto
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.available_locales = [:en, :fr]
+    config.i18n.default_locale = :fr
+
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Configuration for the application, engines, and railties goes here.
