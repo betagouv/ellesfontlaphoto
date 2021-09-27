c95b7d92d8015b143f1ff6b37f1b42503623fa2f
Nadia Auger <nadia.auger0@gmail.com>
2021-09-27 17:20:40 +0200

    modif index

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,13 +1,18 @@
 class HelpsController < ApplicationController
 
   def index
+    @helps = Help.order(start_date: :asc)
+    @searched = false
     if params[:query_residence].present?
-      @helps = Help.search_by_residence_condition([params[:query_residence], "Française"])
+      # @helps = Help.order(start_date: :asc).search_by_residence_condition([params[:query_residence], "Française ou résidant en France"])
+      @helps = Help.where(residence_condition: [params[:query_residence], "Française ou résidant en France"]).order(start_date: :asc)
       @selected = params[:query_residence]
       @searched = true
-    else
-      @helps = Help.all
-      @searched = false
+    end
+    if params[:query_type].present?
+      @helps = @helps.where(help_type: Help::HELP_TYPE.key(params[:query_type]).to_s).order(start_date: :asc)
+      @selected_type = params[:query_type]
+      @searched = true
     end
     @helps_count = @helps.count
   end
