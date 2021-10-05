e9821f282195570cadfe2fe3b5c07c31c8d969cb
Nadia Auger <nadia.auger0@gmail.com>
2021-10-05 13:10:00 +0200

    update db

--- db/migrate/20210920081059_change_columns_to_helps.rb
+++
@@ -1,8 +0,0 @@
-class ChangeColumnsToHelps < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :helps, :end_month, :string
-    remove_column :helps, :start_month, :string
-    add_column :helps, :end_date, :date
-    add_column :helps, :start_date, :date
-  end
-end
