652b1d954fa329370856ec500f5c2c79b8bdd310
Nadia Auger <nadia.auger0@gmail.com>
2021-10-21 18:41:38 +0200

    add some updates on BO helps

--- app/admin/helps.rb
+++ app/admin/helps.rb
@@ -5,7 +5,8 @@ ActiveAdmin.register Help do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :title, :description, :sector, :structure_name, :start_date, :end_date, :help_amount, :residence_condition, :general_condition, :specific_condition, :candidate_url, :institution_url, :selection, :compo_commission, :url_commission, :old_laureat, :old_laureat_url, :help_type, :admin_attachment, :artistic_attachment, :other_attachment, :contact_institution, :contact_institution_url
+
+  permit_params :title, :description, :example_enrollment_url, :residence_time, :sector, :structure_name, :start_date, :end_date, :help_amount, :residence_condition, :general_condition, :specific_condition, :candidate_url, :institution_url, :selection, :compo_commission, :url_commission, :old_laureat, :old_laureat_url, :help_type, :admin_attachment, :artistic_attachment, :other_attachment, :contact_institution, :contact_institution_url
   #
   # or
   #
@@ -28,8 +29,44 @@ ActiveAdmin.register Help do
 
   form do |f|
     f.semantic_errors # shows errors on :base
-    f.inputs          # builds an input field for every attribute
-    f.actions         # adds the 'Submit' and 'Cancel' buttons
+    f.inputs "Bandeau" do
+      f.input :title
+      f.input :description
+      f.input :help_amount
+      f.input :residence_time
+      f.input :start_date, as: :date_picker, input_html: {style: "width:20%"}
+      f.input :end_date, as: :date_picker, input_html: {style: "width:20%"}
+      f.input :residence_condition
+      f.input :general_condition
+    end
+    f.inputs "Candidatez" do
+      f.input :candidate_url
+    end
+    f.inputs "Selection" do
+      f.input :selection, as: :quill_editor
+      f.input :example_enrollment_url
+      f.input :compo_commission, as: :quill_editor
+      f.input :url_commission
+      f.input :old_laureat, as: :quill_editor
+      f.input :old_laureat_url
+    end
+    f.inputs "Pièces à fournir" do
+      f.input :admin_attachment, as: :quill_editor
+      f.input :artistic_attachment, as: :quill_editor
+      f.input :other_attachment, as: :quill_editor
+      f.input :specific_condition, as: :quill_editor
+      f.input :parentality
+      f.input :accessibility
+    end
+    f.inputs "Contact" do
+      f.input :institution_name
+      f.input :institution_url
+      f.inputs :contact_institution
+      f.input :contact_intitution_email
+      f.input :contact_intitution_partenaire
+      f.input :faq_url
+    end
+    f.actions
   end
 
 end
