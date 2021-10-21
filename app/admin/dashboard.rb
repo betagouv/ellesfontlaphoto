d76a7a44edb5042aa075eb3bfe06a16518e69e1a
Nadia Auger <nadia.auger0@gmail.com>
2021-10-21 19:00:06 +0200

    modification back office dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,32 +1,24 @@
 ActiveAdmin.register_page "Dashboard" do
-  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc { "ELLES FONT LA CULTURE BACK OFFICE" }
 
-  content title: proc { I18n.t("active_admin.dashboard") } do
+  content title: proc { "ELLES FONT LA CULTURE BACK OFFICE" } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span "Bienvenue dans le back office choisissez une action :"
+        columns do
+          column do
+            panel "Creer un Conseil/Temoignage Video" do
+              link_to "Creer un Conseil/Temoignage Video", new_admin_conseils_video_path
+            end
+            panel "Creer un Conseil/Temoignage Article" do
+              link_to "Creer un Conseil/Temoignage Article", new_admin_conseils_article_path
+            end
+            panel "Creer une nouvelle Aide" do
+              link_to "Creer une nouvelle Aide", new_admin_help_path
+            end
+          end # content
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
