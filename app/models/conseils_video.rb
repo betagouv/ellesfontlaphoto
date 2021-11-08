c09146b42f41ef0ad85d7eabeb3a0dc39daea73c
Nadia Auger <nadia.auger0@gmail.com>
2021-11-08 16:25:20 +0100

    top 75

--- app/models/conseils_video.rb
+++ app/models/conseils_video.rb
@@ -26,8 +26,8 @@ class ConseilsVideo < ApplicationRecord
     if image.attached? && !image.content_type.in?(%w(image/jpg image/jpeg image/png))
       errors.add(:image, "L'image doit être au format png ou jpeg/jpg")
     end
-    if video.attached? && !video.content_type.in?(%w(video/mp4 video/mpeg))
-      errors.add(:image, "La vidéo doit être au format mp4 ou mpeg")
+    if video.attached? && !video.content_type.in?(%w(video/mp4))
+      errors.add(:video, "La vidéo doit être au format mp4 et ne doit pas depasser 100MB ")
     end
   end
 
