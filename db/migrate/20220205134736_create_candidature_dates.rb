72bf0a67eae133991fc76f95a1091ec23022e2a7
Nadia Auger <nadia.auger0@gmail.com>
2022-02-05 15:14:28 +0100

    runned migration dates reviews

--- db/migrate/20220205134736_create_candidature_dates.rb
+++ db/migrate/20220205134736_create_candidature_dates.rb
@@ -2,8 +2,8 @@ class CreateCandidatureDates < ActiveRecord::Migration[6.1]
   def change
     create_table :candidature_dates do |t|
       t.references :help
-      t.start_date :date
-      t.end_date :date
+      t.date :start_date
+      t.date :end_date
       t.timestamps
     end
   end
