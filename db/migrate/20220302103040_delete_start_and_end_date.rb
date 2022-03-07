a2728a2b46ad5fea87358bd49f5a2f1659c9e152
Nadia Auger <nadia.auger0@gmail.com>
2022-03-07 14:54:26 -0500

    to rollback the remove column

--- db/migrate/20220302103040_delete_start_and_end_date.rb
+++ db/migrate/20220302103040_delete_start_and_end_date.rb
@@ -1,6 +1,6 @@
 class DeleteStartAndEndDate < ActiveRecord::Migration[6.1]
   def change
-    remove_column :helps, :start_date
-    remove_column :helps, :end_date
+    remove_column :helps, :start_date, :date
+    remove_column :helps, :end_date, :date
   end
 end
