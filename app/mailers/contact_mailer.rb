c81e985469bc52d91ed660a70bd4ed83407d1a84
Nadia Auger <nadia.auger0@gmail.com>
2022-03-14 09:48:12 -0400

    ajout de Sarah Witt dans les receveurs des nouveaux contacts

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ApplicationMailer
 
   def new_contact(contact)
     @contact = contact
-    mail_to_send = ["ellesfontlaculture@beta.gouv.fr", "nadia.auger@beta.gouv.fr","lauriane.fort@beta.gouv.fr"]
+    mail_to_send = ["ellesfontlaculture@beta.gouv.fr", "nadia.auger@beta.gouv.fr","lauriane.fort@beta.gouv.fr","sarah.witt@beta.gouv.fr"]
     mail(to: mail_to_send, subject: "[ellesfontlaculture.beta.gouv.fr] NOUVEAU CONTACT !")
   end
 end
