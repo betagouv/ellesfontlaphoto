fe69b4bb3f90950c36664ebcafcc87e56d3fef44
Nadia Auger <nadia.auger0@gmail.com>
2022-05-03 15:25:07 +0200

    ajout du systeme de deselection de valeurs pour les inputs de type select du filtrage des aides

--- app/javascript/controllers/filter_helps_controller.js
+++ app/javascript/controllers/filter_helps_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["form", "inputType", "inputTypePhoto", "switchButton", "switchParite", "inputLieu"];
+  static targets = [
+    "form",
+    "crossLieu",
+    "crossTypePhoto",
+    "inputType",
+    "inputTypePhoto",
+    "switchButton",
+    "switchParite",
+    "inputLieu"];
 
   selectCriteria(event) {
     event.currentTarget.classList.toggle("selected")
@@ -14,6 +22,38 @@ export default class extends Controller {
     this.refreshHelps();
   }
 
+  clearSelectedLieu(){
+    this.crossLieuTarget.style.display = "none"
+    this.inputLieuTarget.classList.remove("hide-arrow")
+    var elements = this.inputLieuTarget.options;
+    for(var i = 0; i < elements.length; i++){
+      elements[i].selected = false;
+    }
+    this.refreshHelps();
+  }
+
+  clearSelectedTypePhoto(){
+    this.crossTypePhotoTarget.style.display = "none"
+    this.inputTypePhotoTarget.classList.remove("hide-arrow")
+    var elements = this.inputTypePhotoTarget.options;
+    for(var i = 0; i < elements.length; i++){
+      elements[i].selected = false;
+    }
+    this.refreshHelps();
+  }
+
+  refreshTypePhoto(event) {
+    event.currentTarget.classList.add("hide-arrow")
+    this.crossTypePhotoTarget.style.display = "block"
+    this.refreshHelps();
+  }
+
+  refreshLieu(event) {
+    event.currentTarget.classList.add("hide-arrow")
+    this.crossLieuTarget.style.display = "block"
+    this.refreshHelps();
+  }
+
   refreshHelps() {
     fetch(`${this.formTarget.action}?type_photo_list=${this.inputTypePhotoTarget.value}&type_list=${this.inputTypeTarget.value}&open=${this.switchButtonTarget.checked}&parite=${this.switchPariteTarget.checked}&residence=${this.inputLieuTarget.value}`, { headers: { 'Accept': 'text/plain' } })
     .then(response => response.text())
@@ -21,4 +61,5 @@ export default class extends Controller {
       document.querySelector("#main-index-helps").outerHTML = data;
     })
   }
+
 }
