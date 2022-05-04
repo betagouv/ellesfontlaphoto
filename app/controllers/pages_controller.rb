f4c6dde28f47f041ef4e492fd40a166980c9e041
Nadia Auger <nadia.auger0@gmail.com>
2022-05-04 10:31:50 +0200

    navbar + page creer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
     @contact = Contact.new
     @helps = Help.all.sort_by {|help| help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.nil? ? Date.today + 9000 : help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.end_date }.first(3)
   end
+
+  def rencontre
+  end
 end
