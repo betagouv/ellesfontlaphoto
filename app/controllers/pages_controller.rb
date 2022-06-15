7e827aba54933b30d71e476d505ddd33e2fedfb7
Nadia Auger <nadia.auger0@gmail.com>
2022-06-15 11:44:48 -0400

    ordre des cartes sur le carousel de la home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@ class PagesController < ApplicationController
   def home
     @contact = Contact.new
     @helps = Help.all.where(visible: true).sort_by {|help| help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.nil? ? Date.today + 9000 : help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.end_date }.first(3)
-    @conseils_articles = ConseilsArticle.order(:created_at).first(3)
-    @conseils_videos = ConseilsVideo.order(:created_at).first(3)
-    @webinaires = Webinaire.order(:created_at).first(3)
+    @conseils_articles = ConseilsArticle.order(created_at: :desc).first(3)
+    @conseils_videos = ConseilsVideo.order(created_at: :desc).first(3)
+    @webinaires = Webinaire.order(created_at: :desc).first(3)
     @contenus = @helps + @conseils_videos + @conseils_articles + @webinaires
     @contenus.sort! do |a, b|
       if b.class == Webinaire
