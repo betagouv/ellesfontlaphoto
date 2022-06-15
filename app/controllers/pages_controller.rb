a7849d74237ca7d42cc838db60bda4da70b4d2b4
Nadia Auger <nadia.auger0@gmail.com>
2022-06-15 10:55:19 -0400

    modification responsive carousel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     @helps = Help.all.where(visible: true).sort_by {|help| help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.nil? ? Date.today + 9000 : help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.end_date }.first(5)
     @conseils_articles = ConseilsArticle.order(:created_at).first(5)
     @conseils_videos = ConseilsVideo.order(:created_at).first(5)
-    @webinaires = Webinaire.order(:created_at).first(5)
+    @webinaires = Webinaire.order(:date).first(5)
     @contenus = @helps + @conseils_videos + @conseils_articles + @webinaires
     @contenus = @contenus.shuffle
   end
