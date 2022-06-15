ed59bf78e9f3374ff11f1c137594090b532fbacc
Nadia Auger <nadia.auger0@gmail.com>
2022-06-15 11:30:17 -0400

    ordre des cartes sur le carousel de la home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,18 @@
 class PagesController < ApplicationController
   def home
     @contact = Contact.new
-    @helps = Help.all.where(visible: true).sort_by {|help| help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.nil? ? Date.today + 9000 : help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.end_date }.first(5)
-    @conseils_articles = ConseilsArticle.order(:created_at).first(5)
-    @conseils_videos = ConseilsVideo.order(:created_at).first(5)
-    @webinaires = Webinaire.order(:created_at).first(5)
+    @helps = Help.all.where(visible: true).sort_by {|help| help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.nil? ? Date.today + 9000 : help.candidature_dates.where("end_date >= ?", Date.today).order("end_date ASC").first.end_date }.first(3)
+    @conseils_articles = ConseilsArticle.order(:created_at).first(3)
+    @conseils_videos = ConseilsVideo.order(:created_at).first(3)
+    @webinaires = Webinaire.order(:created_at).first(3)
     @contenus = @helps + @conseils_videos + @conseils_articles + @webinaires
-    @contenus = @contenus.shuffle
+    @contenus.sort! do |a, b|
+      if b.class == Webinaire
+        b.date <=> a.created_at
+      elsif (b.class == ConseilsArticle) || (b.class == ConseilsVideo) || (b.class == Help)
+        b.created_at <=> a.created_at
+      end
+    end
   end
 
   def rencontre
