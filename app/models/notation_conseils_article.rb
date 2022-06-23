e7d42ee238a6b585cf6c944faff654a187acf106
Nadia Auger <nadia.auger0@gmail.com>
2022-06-23 13:53:44 +0200

    modifications des donnees statistiques + quelques modifications de design

--- app/models/notation_conseils_article.rb
+++ app/models/notation_conseils_article.rb
@@ -4,4 +4,12 @@ class NotationConseilsArticle < ApplicationRecord
   ANSWERS = ["inutile", "utile"]
   SUGGESTIONS_INUTILES = ["J'ai déjà ces connaissances", "C’est trop complexe", "Cela ne me concerne pas", "Je n’en ai pas l’usage", "C’est de mauvaise qualité"]
   SUGGESTIONS_UTILES = ["J’ai appris des éléments", "C’est explicite", "Je vois que je ne suis pas seule"]
+
+  def self.pourcentage_notation_conseils_articles_utile
+    (NotationConseilsArticle.where(utile: 1).count * 100) / NotationConseilsArticle.count
+  end
+
+  def self.pourcentage_notation_conseils_articles_inutile
+    (NotationConseilsArticle.where(inutile: 1).count * 100) / NotationConseilsArticle.count
+  end
 end
--- app/models/notation_help.rb
+++ app/models/notation_help.rb
@@ -3,4 +3,16 @@ class NotationHelp < ApplicationRecord
   SUGGESTIONS_INUTILES = ["L’aide n’est pas ouverte", "Je ne sais pas quoi faire après cette lecture", "C’est trop complexe", "L’aide ne correspond pas à mon profil"]
   SUGGESTIONS_UTILES = ["Je ne connaissais pas cette aide", "Le contenu est explicite", "Je vais candidater", "Je sais qui contacter"]
   ANSWERS = ["yes", "no", "yesbut"]
+
+  def self.pourcentage_notation_help_utile
+    (NotationHelp.where(oui: 1).count * 100) / NotationHelp.count
+  end
+
+  def self.pourcentage_notation_help_inutile
+    (NotationHelp.where(non: 1).count * 100) / NotationHelp.count
+  end
+
+  def self.pourcentage_notation_help_utile_mais_fermee
+    (NotationHelp.where(oui_mais_fermee: 1).count * 100) / NotationHelp.count
+  end
 end
