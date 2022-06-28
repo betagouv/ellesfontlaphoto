9475c50936b8342105c1eeb6031e9f29a087c79b
Nadia Auger <nadia.auger0@gmail.com>
2022-06-28 10:44:26 +0200

    validation presence de la date dans les webinaires obligatoire

--- app/models/webinaire.rb
+++ app/models/webinaire.rb
@@ -2,6 +2,7 @@ class Webinaire < ApplicationRecord
   acts_as_taggable_on :tags, :category, :type
   has_many_attached :images
 
+  validates :date, presence: true
     TYPE_RENCONTRE = [
     "webinaire",
     "outil d'entraide",
