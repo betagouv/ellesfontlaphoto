57dfd7a94fd2b50dc914e6cdfd4520a794612b37
Nadia Auger <nadia.auger0@gmail.com>
2022-07-25 13:06:57 +0200

    carousel chiffres inegalites avec bootstrap done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
-
+import "bootstrap"
 import "chartkick/chart.js"
 
 Rails.start()
