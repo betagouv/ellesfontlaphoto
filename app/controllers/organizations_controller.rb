ddeb5c88459f88a0bfeceb2504b0f3390e28ed75
Nadia Auger <nadia.auger0@gmail.com>
2022-09-02 17:35:03 +0200

    formulaire collecte infos pour index de parite

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,4 +2,18 @@ class OrganizationsController < ApplicationController
   def index
     @organizations = Organization.order("total_nb_expos DESC NULLS LAST")
   end
+
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    raise
+  end
+
+  private
+
+  def organization_params
+    params.permit(:organization).require(:organization_type, :name, :city, :nb_women_dir, :total_nb_dir, :nb_women_expos, :total_nb_expos)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_22_144507) do
+ActiveRecord::Schema.define(version: 2022_09_02_144738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -248,6 +248,7 @@ ActiveRecord::Schema.define(version: 2022_08_22_144507) do
     t.integer "score_parity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "visible"
   end
 
   create_table "reviews", force: :cascade do |t|
