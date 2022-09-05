d263eb9d52e3908fdb574a971d7aa99034afb01d
Nadia Auger <nadia.auger0@gmail.com>
2022-09-05 19:15:30 +0200

    ajout de l'animation du loader

--- app/models/case_review.rb
+++ app/models/case_review.rb
@@ -1,10 +1,10 @@
 class CaseReview < ApplicationRecord
   has_one_attached :case, service: :scaleway
-  # validate :correct_type_attachment
+  validate :correct_type_acttachment
 
-  # def correct_type_attachment
-  #   if case.attached? && !case.content_type.in?(%w(application/pdf))
-  #     errors.add(:case, "Le dossier doit être au format pdf")
-  #   end
-  # end
+  def correct_type_attachment
+    if self.case.attached? && !self.case.content_type.in?(%w(application/pdf))
+      errors.add(:case, "Le dossier doit être au format pdf")
+    end
+  end
 end
