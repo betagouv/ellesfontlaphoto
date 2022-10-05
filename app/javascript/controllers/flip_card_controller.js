eb9e563026a9209fffcac9a5cd905a08a802e860
Nadia Auger <nadia.auger0@gmail.com>
2022-10-05 13:36:01 +0200

    align items card organizations

--- app/javascript/controllers/flip_card_controller.js
+++ app/javascript/controllers/flip_card_controller.js
@@ -4,6 +4,7 @@ export default class extends Controller {
   static targets = ["card"]
 
   flip() {
+    this.cardTarget.style.opacity = "1"
     this.cardTarget.classList.toggle("is-flipped")
   }
 }
