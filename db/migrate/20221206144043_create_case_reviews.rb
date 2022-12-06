73884fab5eb003f25db9c6e1fa93f2d5b0143b19
Nadia AUGER <nadia.auger0@gmail.com>
2022-12-06 15:42:27 +0100

    maj node modules

---
+++ db/migrate/20221206144043_create_case_reviews.rb
@@ -0,0 +1,17 @@
+class CreateCaseReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :case_reviews do |t|
+      t.string :candidate_email
+      t.string :reviewer_email
+      t.boolean :engagement
+      t.text :message
+      t.date :creation_binome
+      t.date :date_notation
+      t.string :status
+      t.text :points_forts
+      t.text :points_faibles
+      t.string :lien_video
+      t.text :autres
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_20_144352) do
+ActiveRecord::Schema.define(version: 2022_12_06_144043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,8 +81,15 @@ ActiveRecord::Schema.define(version: 2022_10_20_144352) do
   create_table "case_reviews", force: :cascade do |t|
     t.string "candidate_email"
     t.string "reviewer_email"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.boolean "engagement"
+    t.text "message"
+    t.date "creation_binome"
+    t.date "date_notation"
+    t.string "status"
+    t.text "points_forts"
+    t.text "points_faibles"
+    t.string "lien_video"
+    t.text "autres"
   end
 
   create_table "conseils_articles", force: :cascade do |t|
