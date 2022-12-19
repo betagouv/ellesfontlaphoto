d8b5a9629b0278b19c168cb4af04d1d8b6b2a870
Nadia AUGER <nadia.auger0@gmail.com>
2022-12-19 17:40:23 +0100

    envoie email notifications

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
 
+  config.action_mailer.default_url_options = { host: "www.ellesfontlaculture.beta.gouv.fr" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
