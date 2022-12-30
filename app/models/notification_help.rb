fb7a7576adecb34258ff420829ebc090dd136b53
Nadia AUGER <nadia.auger0@gmail.com>
2022-12-30 17:39:24 +0100

    ajout validation sur email notification d'aide

--- app/models/notification_help.rb
+++ app/models/notification_help.rb
@@ -1,4 +1,5 @@
 class NotificationHelp < ApplicationRecord
   belongs_to :help
   validates :email, presence: true, uniqueness: { scope: :help }
+  validates :email, format: { with: /\A\S+@.+\.\S+\z/ }
 end
