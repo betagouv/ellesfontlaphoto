fa70a70cf750cf8a472a8b8a5d6482c7c560e0a3
Nadia AUGER <nadia.auger0@gmail.com>
2023-01-11 18:08:38 +0100

    Ajout CGU pour l'ajout d'aide

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -81,6 +81,7 @@ class HelpsController < ApplicationController
       HelpMailer.new_help(@help).deliver_later
       redirect_to helps_confirm_path
     else
+      @error_cgu = !@help.accept_cgu
       render :new
     end
   end
@@ -91,13 +92,13 @@ class HelpsController < ApplicationController
   private
 
   def help_params
-    params.require(:help).permit(:visible, :title, :description, :type, :type_photo, :institution_name, :institution_url, :contact_intitution_email, :permanent, :help_amount, :residence_condition, :general_condition, :specific_condition, :residence_time, :admin_attachment, :admin_attachment, :artistic_attachment, :candidate_url, :selection, :parentality, :accessibility, :author_email, :suggested, :candidature_dates_attributes => [:start_date, :end_date]) do |help_param|
+    params.require(:help).permit(:visible, :title, :description, :type, :type_photo, :institution_name, :institution_url, :contact_intitution_email, :permanent, :help_amount, :residence_condition, :general_condition, :specific_condition, :residence_time, :admin_attachment, :admin_attachment, :artistic_attachment, :candidate_url, :selection, :parentality, :accessibility, :author_email, :suggested, :accept_cgu, :candidature_dates_attributes => [:start_date, :end_date]) do |help_param|
       help_param.require(:author_email)
     end
   end
 
   def help_params_permanent
-    params.require(:help).permit(:visible, :title, :description, :type, :type_photo, :institution_name, :institution_url, :contact_intitution_email, :permanent, :help_amount, :residence_condition, :general_condition, :specific_condition, :residence_time, :admin_attachment, :admin_attachment, :artistic_attachment, :candidate_url, :selection, :parentality, :accessibility, :author_email, :suggested) do |help_param|
+    params.require(:help).permit(:visible, :title, :description, :type, :type_photo, :institution_name, :institution_url, :contact_intitution_email, :permanent, :help_amount, :residence_condition, :general_condition, :specific_condition, :residence_time, :admin_attachment, :admin_attachment, :artistic_attachment, :candidate_url, :selection, :parentality, :accessibility, :author_email, :suggested, :accept_cgu) do |help_param|
       help_param.require(:author_email)
     end
   end
