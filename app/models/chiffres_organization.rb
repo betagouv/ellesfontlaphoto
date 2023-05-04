304e204e9cd15c4074f1b20db4f5df1ef6ed7df7
Nadia AUGER <nadia.auger0@gmail.com>
2023-05-04 23:49:22 +0200

    validations chiffres numericality

--- app/models/chiffres_organization.rb
+++ app/models/chiffres_organization.rb
@@ -22,29 +22,36 @@ class ChiffresOrganization < ActiveRecord::Base
 
   # Validations de calcul de parité
   after_validation :calculate_parity
-  validates :nb_total_exposes_expo_collective, numericality: { greater_than_or_equal_to: :nb_femmes_exposees_expo_collective, allow_nil: true }
-  validates :nb_total_exposes_expo_mono, numericality: { greater_than_or_equal_to: :nb_femmes_exposees_expo_mono, allow_nil: true }
-  validates :nb_total_commissaires, numericality: { greater_than_or_equal_to: :nb_femmes_commissaires, allow_nil: true }
-  validates :nb_total_artistes, numericality: { greater_than_or_equal_to: :nb_femmes_artistes, allow_nil: true }
-  validates :nb_total_oeuvres_photo, numericality: { greater_than_or_equal_to: :nb_total_oeuvres_photo, allow_nil: true }
-  validates :nb_total_oeuvres_ajoutees, numericality: { greater_than_or_equal_to: :nb_femmes_oeuvres_ajoutees, allow_nil: true }
-  validates :nb_total_enseignants, numericality: { greater_than_or_equal_to: :nb_femmes_enseignantes, allow_nil: true }
-  validates :nb_total_etudiants, numericality: { greater_than_or_equal_to: :nb_femmes_etudiantes, allow_nil: true }
-  validates :nb_total_photographes_etudies, numericality: { greater_than_or_equal_to: :nb_femmes_photographes_etudiees, allow_nil: true }
-  validates :nb_total_laureates, numericality: { greater_than_or_equal_to: :nb_femmes_laureates, allow_nil: true }
-  validates :nb_total_candidats, numericality: { greater_than_or_equal_to: :nb_femmes_candidates, allow_nil: true }
-  validates :nb_total_jurys, numericality: { greater_than_or_equal_to: :nb_femmes_jurys, allow_nil: true }
-  validates :nb_total_publies, numericality: { greater_than_or_equal_to: :nb_femmes_publiees, allow_nil: true }
-  validates :nb_total_iconographes, numericality: { greater_than_or_equal_to: :nb_femmes_iconographes, allow_nil: true }
-  validates :nb_total_accueil_residence, numericality: { greater_than_or_equal_to: :nb_femmes_accueil_residence, allow_nil: true }
-  validates :nb_total_photo_ouvrages, numericality: { greater_than_or_equal_to: :nb_femmes_photo_ouvrages, allow_nil: true }
-  validates :nb_total_photographes_invites, numericality: { greater_than_or_equal_to: :nb_femmes_photographes_invites, allow_nil: true }
-  validates :nb_total_directeurs, numericality: { greater_than_or_equal_to: :nb_femmes_directrices, allow_nil: true }
-  validates :nb_total_employes, numericality: { greater_than_or_equal_to: :nb_femmes_employees, allow_nil: true }
+  # before_validation :check_coherence
+  validates :nb_total_exposes_expo_collective, numericality: { greater_than_or_equal_to: :nb_femmes_exposees_expo_collective }, if: -> { !nb_femmes_exposees_expo_collective.nil? && !nb_total_exposes_expo_collective.nil? }
+  validates :nb_total_exposes_expo_mono, numericality: { greater_than_or_equal_to: :nb_femmes_exposees_expo_mono }, if: -> { !nb_total_exposes_expo_mono.nil? && !nb_femmes_exposees_expo_mono.nil? }
+  validates :nb_total_commissaires, numericality: { greater_than_or_equal_to: :nb_femmes_commissaires }, if: -> { !nb_total_commissaires.nil? && !nb_femmes_commissaires.nil? }
+  validates :nb_total_artistes, numericality: { greater_than_or_equal_to: :nb_femmes_artistes }, if: -> { !nb_total_artistes.nil? && !nb_femmes_artistes.nil? }
+  validates :nb_total_oeuvres_photo, numericality: { greater_than_or_equal_to: :nb_total_oeuvres_photo }, if: -> { !nb_total_oeuvres_photo.nil? && !nb_total_oeuvres_photo.nil? }
+  validates :nb_total_oeuvres_ajoutees, numericality: { greater_than_or_equal_to: :nb_femmes_oeuvres_ajoutees }, if: -> { !nb_total_oeuvres_ajoutees.nil? && !nb_femmes_oeuvres_ajoutees.nil? }
+  validates :nb_total_enseignants, numericality: { greater_than_or_equal_to: :nb_femmes_enseignantes }, if: -> { !nb_total_enseignants.nil? && !nb_femmes_enseignantes.nil? }
+  validates :nb_total_etudiants, numericality: { greater_than_or_equal_to: :nb_femmes_etudiantes }, if: -> { !nb_total_etudiants.nil? && !nb_femmes_etudiantes.nil? }
+  validates :nb_total_photographes_etudies, numericality: { greater_than_or_equal_to: :nb_femmes_photographes_etudiees }, if: -> { !nb_total_photographes_etudies.nil? && !nb_femmes_photographes_etudiees.nil? }
+  validates :nb_total_laureates, numericality: { greater_than_or_equal_to: :nb_femmes_laureates }, if: -> { !nb_total_laureates.nil? && !nb_femmes_laureates.nil? }
+  validates :nb_total_candidats, numericality: { greater_than_or_equal_to: :nb_femmes_candidates }, if: -> { !nb_total_candidats.nil? && !nb_femmes_candidates.nil? }
+  validates :nb_total_jurys, numericality: { greater_than_or_equal_to: :nb_femmes_jurys }, if: -> { !nb_total_jurys.nil? && !nb_femmes_jurys.nil? }
+  validates :nb_total_publies, numericality: { greater_than_or_equal_to: :nb_femmes_publiees }, if: -> { !nb_total_publies.nil? && !nb_femmes_publiees.nil? }
+  validates :nb_total_iconographes, numericality: { greater_than_or_equal_to: :nb_femmes_iconographes }, if: -> { !nb_total_iconographes.nil? && !nb_femmes_iconographes.nil? }
+  validates :nb_total_accueil_residence, numericality: { greater_than_or_equal_to: :nb_femmes_accueil_residence }, if: -> { !nb_total_accueil_residence.nil? && !nb_femmes_accueil_residence.nil? }
+  validates :nb_total_photo_ouvrages, numericality: { greater_than_or_equal_to: :nb_femmes_photo_ouvrages }, if: -> { !nb_total_photo_ouvrages.nil? && !nb_femmes_photo_ouvrages.nil? }
+  validates :nb_total_photographes_invites, numericality: { greater_than_or_equal_to: :nb_femmes_photographes_invites }, if: -> { !nb_total_photographes_invites.nil? && !nb_femmes_photographes_invites.nil? }
+  validates :nb_total_directeurs, numericality: { greater_than_or_equal_to: :nb_femmes_directrices }, if: -> { !nb_total_directeurs.nil? && !nb_femmes_directrices.nil? }
+  validates :nb_total_employes, numericality: { greater_than_or_equal_to: :nb_femmes_employees }, if: -> { !nb_total_employes.nil? && !nb_femmes_employees.nil? }
+
+  # def check_coherence
+  #   raise
+  #   unless nb_total_exposes_expo_collective.nil? || nb_femmes_exposees_expo_collective.nil?
+  #     add_error(nb_total_exposes_expo_collective, nb_femmes_exposees_expo_collective)
+  #   end
+  # end
 
   # Calcul du score de parité
   def calculate_parity
-    # raise
     if organization.orga?
       self.exposes_expo_collective_parite = calculate(nb_femmes_exposees_expo_collective, nb_total_exposes_expo_collective)
       self.exposes_expo_mono_parite = calculate(nb_femmes_exposees_expo_mono, nb_total_exposes_expo_mono)
@@ -75,6 +82,10 @@ class ChiffresOrganization < ActiveRecord::Base
   end
 
   private
+
+  def add_error(nb_total, nb_women)
+    self.errors.add(nb_total, "Doit être supérieur à #{nb_women}")
+  end
 
   def calculate(nb_women, nb_total)
     if nb_total.to_i && nb_women.to_i
