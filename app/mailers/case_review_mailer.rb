bf0d0d8c3b5ace12da6e343bca8afd1c1ad00604
Nadia AUGER <nadia.auger0@gmail.com>
2023-05-21 19:28:45 +0200

    ssmall modification pour case review

--- app/mailers/case_review_mailer.rb
+++ app/mailers/case_review_mailer.rb
@@ -17,7 +17,7 @@ class CaseReviewMailer < ApplicationMailer
   def relance_j7(case_review)
     @case_review = case_review
     case_attachment = URI.open(case_review.case_attachment.url)
-    attachments[case_review.case_attachment.filename.to_s.gsub(/[^0-9A-Za-z]/, '')] = File.read(case_attachment)
+    attachments[case_review.case_attachment.filename.to_s.gsub(/[^0-9A-Za-z.]/, '')] = File.read(case_attachment)
     mail(to: case_review.reviewer_email, cc: ["revue-dossiers@beta.gouv.fr"], subject: 'Revue de dossiers - plus que 7 jours pour partager vos retours !')
   end
 
@@ -25,7 +25,7 @@ class CaseReviewMailer < ApplicationMailer
     @case_review = case_review
     @reviewer = case_review.reviewer_email
     case_attachment = URI.open(case_review.case_attachment.url)
-    attachments[case_review.case_attachment.filename.to_s.gsub(/[^0-9A-Za-z]/, '')] = File.read(case_attachment)
+    attachments[case_review.case_attachment.filename.to_s.gsub(/[^0-9A-Za-z.]/, '')] = File.read(case_attachment)
     mail(to: @reviewer, cc: ["revue-dossiers@beta.gouv.fr"], subject: 'Revue de dossiers - À vous de jouer !')
   end
 
@@ -33,7 +33,7 @@ class CaseReviewMailer < ApplicationMailer
     @case_review = case_review
     if case_review.review_comment.attached?
       attachment = URI.open(case_review.review_comment.url)
-      attachments[case_review.review_comment.filename.to_s.gsub(/[^0-9A-Za-z]/, '')] = File.read(attachment)
+      attachments[case_review.review_comment.filename.to_s.gsub(/[^0-9A-Za-z.]/, '')] = File.read(attachment)
     end
     mail(to: @case_review.candidate_email, cc: ["revue-dossiers@beta.gouv.fr"], subject: 'Revue de dossiers - découvrez les retours de votre binôme !')
   end
