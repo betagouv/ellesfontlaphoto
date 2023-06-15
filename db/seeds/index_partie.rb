707bf9cbeaecdffe3bfce818dbf27d128dcd3ab3
Nadia AUGER <nadia.auger0@gmail.com>
2023-06-15 21:59:47 +0200

    modif seed

--- db/seeds/index_partie.rb
+++ db/seeds/index_partie.rb
@@ -12,30 +12,33 @@ CSV.foreach(filepath, headers: :first_row) do |row|
   )
   new_chiffre.organization = organization
   new_chiffre.save
+  p "chiffre organization pour #{organization.name} ajouté"
 end
 
 
-filepath = File.join(Rails.root, 'db', 'nouvelles_orga.csv')
+# filepath = File.join(Rails.root, 'db', 'nouvelles_orga.csv')
 
-CSV.foreach(filepath, headers: :first_row) do |row|
-  new_orga = Organization.new(
-    name: row["name"],
-    organization_type: row["organization_type"],
-    city: row["city"]
-  )
-  new_orga.save
-end
+# CSV.foreach(filepath, headers: :first_row) do |row|
+#   new_orga = Organization.new(
+#     name: row["name"],
+#     organization_type: row["organization_type"],
+#     city: row["city"]
+#   )
+#   new_orga.save
+#   p "nouvelle organization #{new.name} ajouté"
+# end
 
 filepath = File.join(Rails.root, 'db', 'datas_nouvelles_orga.csv')
 
 CSV.foreach(filepath, headers: :first_row) do |row|
   organization = Organization.where(name: row["name"]).first
   chiffre_orga = ChiffresOrganization.new(
-    anne: row["annee"].to_i,
+    annee: row["annee"].to_i,
     nb_femmes_exposees_expo_collective: row["nb_femmes_exposees_expo_collective"].to_i,
     nb_total_exposes_expo_collective: row["nb_total_exposes_expo_collective"].to_i,
     nb_femmes_directrices: row["nb_femmes_directrices"].to_i,
     nb_total_directeurs: row["nb_total_directeurs"].to_i
   )
   chiffre_orga.organization = organization
+  p "chiffre organization pour #{chiffre_orga.name} ajouté"
 end
